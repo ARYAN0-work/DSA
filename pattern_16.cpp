d773a8b86bc2de12dfc9dfd14fd61da55c654b3a
Aryan Mishra <carrer1270@gmail.com>
2026-03-05 18:24:45 +0000

    fix(pattern): fixed the problem of  pattern16 soln

--- pattern_16.cpp
+++ pattern_16.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void pattern16(int n){
            for (int i = 1; i <= n; i++)
            {
-            for (int j = 1; j < i; j++)
+            for (int j = 0; j < i; j++)
             {
                 cout<< char('A'+j);
             }
