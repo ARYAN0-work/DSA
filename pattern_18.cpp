84c980fe6d9b9f4fab3786684dc472214957bf98
Aryan Mishra <carrer1270@gmail.com>
2026-03-06 15:31:20 +0000

    feat(pattern): added soln of  pattern18 soln

---
+++ pattern_18.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print18(int n){
+     for (int i = 1; i <= n; i++)
+     {
+        char start ='A' + (n-i);
+
+        for (char ch = start; ch <= 'A' +n -i; ch++)
+        {
+            cout<< ch;
+        }
+        cout<< endl;
+     }
+     
+}
+
+
+int main(){
+    int n =5;
+    print18(n);
+}
