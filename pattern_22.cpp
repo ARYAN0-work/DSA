89bdb03bb44864572930ca17cc4b3920635a34f7
Aryan Mishra <carrer1270@gmail.com>
2026-03-08 18:11:43 +0000

    feat(pattern): added soln of pattern 22

--- pattern_22.cpp
+++ pattern_22.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 void print20(int n){
+             
+          for (int i = 0; i < 2*n-1; i++)
+          {
+            for (int j = 0; j < 2*n-1; j++)
+            {
+                int top = i;
+                int left = j;
+                int bottom = (2*n-2) - i;
+                int right = (2*n-2) -j;
+                
+                int distance = min(min(top,bottom), min(left,right));
 
+                cout << n - distance << " ";
+            }
+            cout << endl;
+          }
+          
+       
 }
 
 int main(){
-    int n =5;
+    int n =4;
     print20(n);
 }
