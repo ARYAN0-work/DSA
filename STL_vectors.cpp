6f6e8fd2227eca295b43383f622f11813039d41b
Aryan Mishra <carrer1270@gmail.com>
2026-03-18 13:07:42 +0000

    adding deque topic(STL)

--- STL_vectors.cpp
+++ STL_vectors.cpp
@@ -132,3 +132,21 @@ void explainList(){
     // begin, end, rbegin, rend, clear, insert, size, swap => all similar
 }
 
+// => exactly similar to LIST & VECTORS
+void explainDegue(){ 
+    deque<int>dq;
+    dq.push_back(1); // {1}
+    dq.emplace_back(2); // {1,2}
+    dq.push_front(4); // {3, 4, 1, 2}
+
+    dq.pop_back(); // {3,4,1}'
+    dq.pop_front(); // {4,1}
+
+    dq.back();
+
+    dq.front();
+
+    //rest functions same as vector
+    //begin, end, rbegin, rend, clear, insert, size, swap 
+}
+
