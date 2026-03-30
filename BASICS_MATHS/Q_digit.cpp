1802a778146f3984c886dcd839236c652c01a568
ARYAN0-WORK <carrer1270@gmail.com>
2026-03-30 17:20:18 +0000

    adding notes of count digits

---
+++ BASICS_MATHS/Q_digit.cpp
@@ -0,0 +1,86 @@
+/*
+Problem: 
+
+Given an integer 'n', return the number of digits present in the number.
+
+Example:
+Input: n = 156
+Output: 3
+
+Explanation:
+156 has 3 digits.
+
+*/
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+void countDigit(int n){
+    while (n>0)
+    {
+        int number = n% 10;
+        int digit1 = n/10;
+    }
+    
+    cout<< 
+}
+
+int main() {
+    int n;
+    cin>> n;
+    countDigit(n);
+}
+
+*/
+
+// Just put the cout variable her the no. of times the extraction happens that is the number of times the digit will be i can say cout = cout +1 the logic of extraction of digits if i can do this can i say i will ne the able to count the no.
+
+// another way to find log10(7789) = 3.81
+// 3.81 + 1 = 4.89[ integer =4] ANS
+
+// TC = O(log10(N)) => you sae the n is going to didvisible by 10 how many times is it gonna divisible by 10  => TC [lastdigit, cout = unit operation=> no. very large] = O(log10(N)) 
+
+// one  more thing if no. is divible by 5 => TC = O(log5(N))
+
+//if no. is divible by 7 => TC = O(log7(N))
+
+//=> if no. of iteration is based on division[n=n/10] the tc will be log
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+void countDigit(int n){
+    int cnt = (int)(log10(n));
+}
+
+int main() {
+    int n;
+    cin>> n;
+    countDigit(n);
+}
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void countDigit(int n){
+    int cnt =0;
+    while (n>0)
+    {
+        int lastDigit =n %10;
+        cnt = cnt + 1;
+        n= n/10;
+    }
+    //  return cnt;     mistake void hai return nhi karega change to int countDigit or do this =>
+
+    cout<< cnt;
+}
+
+int main() {
+    int n;
+    cin>> n;
+    countDigit(n);
+}
