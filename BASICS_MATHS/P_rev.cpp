4f301b50b6250f7c961fd1e1b4ce7dfeb06f700a
ARYAN0-WORK <carrer1270@gmail.com>
2026-04-09 12:09:14 +0000

    adding practice soln for rev. of a no

---
+++ BASICS_MATHS/P_rev.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void digit(int n){
+    int count;
+    while(n>0){
+     int rem = n%10;
+     n= n/10;
+
+    int count= (rem*10) + rem;
+    }
+    cout << count;
+}
+
+int main(){
+  int n;
+  cin>> n;
+  digit(n);
+}
+
+/*  => logic feel ho gya but how did they made the formula  see that
+145
+
+n%10
+
+5 c
+
+digit = n*10 + c
+*/
+
+// theres a lot of problem in this see again 
