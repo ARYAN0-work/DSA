04ae7761dc83466a4bb987351a6d1543e51b0cd0
ARYAN0-WORK <carrer1270@gmail.com>
2026-04-09 12:40:39 +0000

    adding solution of palindrome problem

---
+++ BASICS_MATHS/Q_palindrome.cpp
@@ -0,0 +1,42 @@
+/*
+Palindrome Number
+
+Problem Statement:
+
+Write a program to determine if the given number is a palindrome or not.
+Print true if it is a palindrome, false otherwise.
+
+A palindrome number is a number for which the reverse is exactly the same as the original number.
+
+Example:
+121 → Palindrome
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int palindrome(int n){
+    int dup =n;
+    int count = 0;
+    while(n>0){
+      int remainder= n%10;
+      n=n/10;
+
+      count = (count*10) + remainder;
+    }
+    if (dup == count)
+    {
+        cout << "true";
+    }
+    else
+    {
+        cout<< "false";
+    }
+    return 0;
+}
+
+int main(){
+    int n;
+    cin>> n;
+    palindrome(n);
+}
