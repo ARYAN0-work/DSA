2c445486db01ee10f5d054eed6084d704a95651b
ARYAN0-WORK <carrer1270@gmail.com>
2026-04-15 15:31:40 +0000

    adding soln of GCD PROBLEM

--- BASICS_MATHS/GCD.cpp
+++ BASICS_MATHS/GCD.cpp
@@ -14,7 +14,7 @@ n1=9  & n2=12
 gcd=1
 
 for(i=1;i<=n;i++){
- if(n1%i==0 &7 n2%i==0)
+ if(n1%i==0 && n2%i==0)
  gcd =i;
 }
 
@@ -31,7 +31,7 @@ ANOTHER METHOD MORE USEFUL=>
 
 N1=20, N2 =40
 
-for(i=min(n1,n2); && n2%i ==0)
+for(i=min(n1,n2);i>=1;i--)
  {
   if(n1%i==0 && n2%i==0){
   print(i)
@@ -47,3 +47,29 @@ for(i=min(n1,n2); && n2%i ==0)
 
  and for tc always take worst case
 */ 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void gcd(int n1,int n2){ // int n1,n2 wrong  dono ko declare
+    int gcd;
+    for (int i = 1; i <= min(n1,n2); i++)
+    {
+
+        if (n1%i==0 && n2%i==0)
+        {
+             gcd=i;
+        }
+        
+    }
+    cout<< gcd<< " "; // if line 57 remooved then yha error bcz of scoping prob
+    
+}
+
+
+int main(){
+    int n1,n2;
+    cin>>n1 ;
+    cin>>n2;
+    gcd(n1,n2);
+}
