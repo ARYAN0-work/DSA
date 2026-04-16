843752a0c8d7b4831b57863f1d3b737fd96dccfc
ARYAN0-WORK <carrer1270@gmail.com>
2026-04-16 14:50:02 +0000

    adding soln of EUCLODIAN therom

--- BASICS_MATHS/EUCLODIAN.cpp
+++ BASICS_MATHS/EUCLODIAN.cpp
@@ -28,3 +28,26 @@ else      print(a)
 
 TC=>O(logfie(min(a,b))) => the reason is simoke we are taking min so thats why fie ,log bcz you reducing no. by divison continuosly
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void gcd(int n1, int n2){
+
+    while(n1 > 0 && n2 > 0){
+        if(n1 > n2){
+            n1 = n1 % n2;
+        } else {
+            n2 = n2 % n1;
+        }
+    }
+
+    if(n1 == 0) cout << n2;
+    else cout << n1;
+}
+
+int main(){
+    int n1, n2;
+    cin >> n1 >> n2;
+    gcd(n1, n2);
+}
