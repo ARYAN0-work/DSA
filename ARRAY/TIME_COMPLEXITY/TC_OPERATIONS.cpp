0b153012ab47a85993d356ed6c58d48ece07cb3a
ARYAN0-WORK <carrer1270@gmail.com>
2026-05-28 19:04:17 +0000

    time complexity related to array

---
+++ ARRAY/TIME_COMPLEXITY/TC_OPERATIONS.cpp
@@ -0,0 +1,26 @@
+/*
+// ARRAY OPERATION 
+
+a   b    c     z    z     <-  array
+0   1    2     3    4     <-  indexs
+
+    operation        :     TC[best/worst]
+1.  accesing         :     O(1)     accesing any element  
+2.  insert           :     O(1)/O(n)    insert any element
+3.  delete           :     O(1)/O(n)    delete any element     
+4.  transvers        :     O(n)         just go element by element     
+5.  search           :  liner-> O(n)  |  binary ->O(logn)         search an element
+
+Benefits:
+1. Random access — O(1)
+2. Cache friendly
+3. Ease of sorting
+4. Implement other D.S.
+
+Limitations:
+1. Fixed size
+2. O(n) — insert/delete
+3. Inefficient for frequent modifications
+
+
+*/
