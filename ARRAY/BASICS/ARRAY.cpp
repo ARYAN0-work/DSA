e7ea3755e93868b56e898ca714ded24846672c12
ARYAN0-WORK <carrer1270@gmail.com>
2026-05-28 19:01:50 +0000

    adding basics of array

---
+++ ARRAY/BASICS/ARRAY.cpp
@@ -0,0 +1,8 @@
+/*
+it is nothing a continuous block of memo that is currently stored inside your computer, now inside continuos block of memo, you can store dta type
+
+RESTRICTION=>
+
+1. fix data type => size of block
+2. everything definded by index pstn => memory location 
+*/
---
+++ ARRAY/BASICS/where_to_use.cpp
@@ -0,0 +1,7 @@
+/**
+ * implement other DATA-STRUCTURE
+ * caching-mamorization-tabulation
+ * graph problem or tree problem and you need to keep visiting nodes 
+ * mathmatical computation
+ * it have coding patterns
+ */
