cdaa4c2e72b32ff6d2fc85af0c65ee445185928c
ARYAN0-WORK <carrer1270@gmail.com>
2026-08-04 16:39:24 +0000

    adding brute force soln of max-subarray

---
+++ ARRAY/MEDIUM/Max-subarray-sum/brute.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int maxi = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            for (int j = i; j < nums.size(); j++) {
+
+                int sum = 0;
+
+                for (int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+
+                maxi = max(maxi, sum);
+            }
+        }
+
+        return maxi;
+    }
+};
