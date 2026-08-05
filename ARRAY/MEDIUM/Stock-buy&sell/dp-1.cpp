88536e385aeeca25333c71d93a6a7d542510d2d0
ARYAN0-WORK <carrer1270@gmail.com>
2026-08-05 18:24:45 +0000

    adding soln for buying and selling stock

---
+++ ARRAY/MEDIUM/Stock-buy&sell/dp-1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int maxProfit=0;
+        int n=pricess.size();
+
+        for(int i=1;i<n;i++){
+            int cost = prices[i]-mini;
+            maxProfit=max(maxProfit,cost);
+            mini=min(mini,prices[i]);
+        }
+        return maxProfit;
+    }
+};
