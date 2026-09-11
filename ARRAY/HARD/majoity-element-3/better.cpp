ab898fb5a6e90d5c5161b570c4a5ce627ef2e46b
ARYAN0-WORK <carrer1270@gmail.com>
2026-09-11 15:51:11 +0000

    adding better soln of majority-element-3 problem

---
+++ ARRAY/HARD/majoity-element-3/better.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+        vector<int>ls;
+        map<int,int>mpp;
+        int  limit = ((nums.size()/3)+1);
+
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+
+            if(mpp(nums[i])==limit){
+              ls.push_back(nums[i]);
+            }
+
+            if(ls.size()==2){
+                break;
+            }
+        }
+        return ls;
+    }
+};
