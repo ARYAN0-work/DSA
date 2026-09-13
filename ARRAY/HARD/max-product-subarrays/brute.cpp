ee096af3ec74523df8c6865fb675bb2551a32eef
ARYAN0-WORK <carrer1270@gmail.com>
2026-09-13 13:19:10 +0000

    adding brute soln of max-product

---
+++ ARRAY/HARD/max-product-subarrays/brute.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+for(int i = 0; i < nums.size(); i++) {
+    for(int j = i; j < nums.size(); j++) {
+
+        int product = 1;
+
+        for(int k = i; k <= j; k++) {
+            product *= nums[k];
+        }
+
+        maxi = max(maxi, product);
+    }
+}
+}
+}
